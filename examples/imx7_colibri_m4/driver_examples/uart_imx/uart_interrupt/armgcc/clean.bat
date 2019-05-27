0c1a26b7b0bce0fe6550f9eb56d2728641a3f940
Andy Kiser <andy.kiser@toradex.com>
2019-05-27 11:17:05 +0200

    Fix clean.bat batch file: Folder /CMakeFiles/ was not removed.

--- examples/imx7_colibri_m4/driver_examples/uart_imx/uart_interrupt/armgcc/clean.bat
+++ examples/imx7_colibri_m4/driver_examples/uart_imx/uart_interrupt/armgcc/clean.bat
@@ -1,9 +1,9 @@
 cd debug
-rd /s /Q CMakeFiles/
+rd /s /Q CMakeFiles
 del /s /Q /F Makefile cmake_install.cmake CMakeCache.txt *.elf *.bin *.map *.hex
 cd ..
 cd release
-rd /s /Q CMakeFiles/
+rd /s /Q CMakeFiles
 del /s /Q /F Makefile cmake_install.cmake CMakeCache.txt *.elf *.bin *.map *.hex
 cd ..
 pause
