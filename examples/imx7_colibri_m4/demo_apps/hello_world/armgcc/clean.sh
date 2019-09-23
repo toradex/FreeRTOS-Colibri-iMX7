5eab7b28f1f7ffa4182b4e904b8a3c414d7ba8d5
Andy Kiser <andy.kiser@toradex.com>
2019-09-23 14:09:20 +0200

    examples: add eclipse generator
    Adding Eclipse CDT4 Generator in orther to have the .cproject .project created with cmake command.
    It help to include the files inside eclipse.
    
    Signed-off-by: Raul Munoz <raul.munoz@toradex.com>
    [add generated Eclipse file to gitignore]
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- examples/imx7_colibri_m4/demo_apps/hello_world/armgcc/clean.sh
+++ examples/imx7_colibri_m4/demo_apps/hello_world/armgcc/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd debug
-rm -rf Makefile cmake_install.cmake CMakeCache.txt CMakeFiles *.elf *.bin *.map *.hex
+rm -rf Makefile cmake_install.cmake CMakeCache.txt CMakeFiles *.elf *.bin *.map *.hex .cproject .project
 cd ..
 cd release
 rm -rf Makefile cmake_install.cmake CMakeCache.txt CMakeFiles *.elf *.bin *.map *.hex
--- examples/imx7_colibri_m4/demo_apps/hello_world/armgcc/debug/build_debug.sh
+++ examples/imx7_colibri_m4/demo_apps/hello_world/armgcc/debug/build_debug.sh
@@ -8,5 +8,5 @@ ARMGCC_CMAKE=$BSPROOT/tools/cmake_toolchain_files/armgcc.cmake
 
 echo "\e[7mBuilding $SCRIPTPATH\e[0m"
 
-cmake -DCMAKE_TOOLCHAIN_FILE="$ARMGCC_CMAKE" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  ..
+cmake -DCMAKE_TOOLCHAIN_FILE="$ARMGCC_CMAKE" -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  ..
 make -j4
